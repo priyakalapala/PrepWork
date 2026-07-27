9271c908825a40593c3556161b1205b69362f998
priyakalapala <priyakalapala@gmail.com>
2026-07-27 11:48:57 -0700

    created Jenkins Conditions

---
+++ Jenkins Conditions.py
@@ -0,0 +1,128 @@
+pipeline {
+    agent any
+    environment {
+        APP_ENV = "Kartheek"
+        AWS_REGION = "Us- east -1"
+    }
+
+    stages {
+        stage("build") {
+            steps {
+                script {
+                    echo "Building in $(APP_ENV) environment"
+                    env.APP_ENV2 = "Priya2"
+                    if(env.APP_ENV2 == "Priya") {
+                    echo "Hello $(env.APP_ENV2)!"
+                    }
+                    esle {
+                        echo "user not recognised"
+                    }
+                     for (int i = 1; i <= 4; i++) {
+                         echo "i = ${i}"
+                     }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+pipeline {
+    agent any
+
+    environment {
+        APP_ENV = "DEV"
+    }
+
+    stages {
+        stage ('Environment Check') {
+            steps {
+                script {
+                     if (env.APP_ENV == "DEV") {
+                        echo "Deploying to Development"
+                    } else {
+                        echo "Deploying to Production"
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
+pipeline {
+    agent any
+    stages {
+        stage("Loop Example") {
+            steps {
+                script {
+                    for (int i =1; i<=5; i++) {
+                                echo "i = ${i}"
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Servers') {
+            steps {
+                script {
+
+                    def servers = ["Dev", "QA", "UAT", "Prod"]
+
+                    for (server in servers) {
+                        echo "Deploying to ${server}"
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+pipeline {
+    agent any
+     stages {
+        stage('Applications') {
+            steps {
+            script {
+                 def apps = ["Billing", "Payment", "Orders"]
+                 apps.each{
+                    echo "Building ${it}"
+                 }
+                 }
+            }
+        }
+     }
+}
+
+
+pipeline {
+    agent any
+
+    stages {
+        stage('While Loop') {
+            steps {
+                script {
+
+                    int i = 1
+                    while (i <= 4) {
+                        echo "Count ${i}"
+                        i++
+                    }
+
+                }
+            }
+        }
+    }
+}
