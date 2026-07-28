a0bd567f891f56975c9e51f02608671f197e6d3c
priyakalapala <priyakalapala@gmail.com>
2026-07-28 11:15:17 -0700

    created file Jenkins loops

---
+++ Jenkins loops.py
@@ -0,0 +1,61 @@
+pipeline {
+    agent any
+    stages {
+        stage('Nested Loop') {
+            steps {
+                script {
+
+                    for (int i = 1; i <= 3; i++) {
+                        for (int j = 1; j <= 2; j++) {
+                            echo "i=${i}, j=${j}"
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Even count') {
+            steps {
+                script {
+                    def numbers = [10, 15, 20, 25, 30]
+                    int count = 0
+                    for (num in numbers) {
+                         if (num % 2 == 0) {
+                            count++
+                        }
+
+                    }
+                        echo "Even Count = ${count}"
+                    
+                }
+            }
+        }
+    }
+}
+
+
+pipeline {
+    agent any
+    stages {
+        stage('while loop') {
+            steps {
+                script {
+                    int i=1
+                    while (i<=3) {
+                        echo "count ${i}"
+                        i++
+                    }
+                }
+            }
+        }
+    }
+}
