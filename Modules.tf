283c2724c25e2ff96a4804af6502c1e321dd7ea0
priyakalapala <priyakalapala@gmail.com>
2026-08-05 11:13:12 -0700

    added text to modules .tf

--- Modules.tf
+++ Modules.tf
@@ -13,3 +13,44 @@ variable "instance_type" {
 }
 
 
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability zones" {
+  description = List of availability zones
+  type = list(string)
+}
+
+variable "instances" {
+  default = {
+    web    = "t3.micro"
+    app    = "t3.small"
+    db     = "t3.medium"
+  }
+}
+
+
+resource "aws_instance" "ec2" {
+  for_each      = var.instances
+  ami           = "ami-0abcd1234"
+  instance_type = each.value
+
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "server_count" {
+  description = "Number of EC2 instances"
+  type = Number
+  default = 2
+}
+
+resource "aws_instance" "web" {
+  count = var.server_count
+  ami = "ami -"
+  instance_type = "t2.micro"
+}
